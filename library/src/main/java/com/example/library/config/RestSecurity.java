15182a8e4e12c29d5c1275ee34524b482a423216
Unknown <R19233@student.osu.cz>
2022-01-30 20:52:55 +0100

    password encoder

--- library/src/main/java/com/example/library/config/RestSecurity.java
+++ library/src/main/java/com/example/library/config/RestSecurity.java
@@ -2,18 +2,20 @@ package com.example.library.config;
 
 import com.example.library.filter.JwtFilter;
 import com.example.library.service.MyUserDetailsService;
+import com.example.library.encoder.AppUserPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,15 +23,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class RestSecurity extends WebSecurityConfigurerAdapter {
 
-    @Autowired
     private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    public RestSecurity(MyUserDetailsService myUserDetailsService){
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+//    @Autowired
+//    private MyUserDetailsService myUserDetailsService;
 
     @Autowired
     private JwtFilter jwtFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+        auth.authenticationProvider(daoAuthenticationProvider());
+//        auth.userDetailsService(myUserDetailsService);
     }
 
     @Override
@@ -56,11 +65,23 @@ public class RestSecurity extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public AuthenticationManager getAuthentication() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(appUserPasswordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(myUserDetailsService);
+        return daoAuthenticationProvider;
+    }
+    @Bean
+    public AppUserPasswordEncoder appUserPasswordEncoder(){
+        return new AppUserPasswordEncoder();
     }
 }
--- library/src/main/java/com/example/library/service/MyUserDetailsService.java
+++ library/src/main/java/com/example/library/service/MyUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.example.library.service;
 
+import com.example.library.encoder.AppUserPasswordEncoder;
 import com.example.library.entity.AppuserEntity;
 import com.example.library.repository.AppuserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,8 +20,30 @@ import java.util.Optional;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+
+    private AppUserPasswordEncoder appUserPasswordEncoder;
+    private AppuserRepository appuserRepository;
+
     @Autowired
-    AppuserRepository appuserRepository;
+    public MyUserDetailsService(AppuserRepository appuserRepository,
+                       @Lazy AppUserPasswordEncoder appUserPasswordEncoder){
+        this.appuserRepository = appuserRepository;
+        this.appUserPasswordEncoder = appUserPasswordEncoder;
+    }
+
+    public AppuserEntity registerUser(AppuserEntity user) {
+        AppuserEntity newUser = new AppuserEntity();
+        newUser.setName(user.getName());
+        newUser.setPassword(appUserPasswordEncoder.encode(user.getPassword()));
+        newUser.setRoles(user.getRoles());
+        appuserRepository.save(newUser);
+        return newUser;
+    }
+
+    public AppuserEntity findUserById(String name) {
+        return appuserRepository.findById(name)
+                .orElseThrow(() -> new NullPointerException("user not found"));
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
